82999a5b97bf5a49518377bc8f178cd3ebb17a59
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-07 10:00:26 +0530

    gsap to,from , repeat, yoyo

--- script.js
+++ script.js
@@ -18,6 +18,27 @@ gsap.to("#box3", {
     backgroundColor:"red",
     borderRadius:"50%",
     scale:0.3,
+    // ease:"bounce"
+    yoyo:true,
+    repeat:-1
+})
+
+gsap.from("h1", {
+    y:20,
+    opacity:0,
+    color:"yellow",
+    duration:2,
+    delay:1,
+    repeat:-1,
     ease:"bounce"
-}
-)
+})
+
+gsap.from("h2", {
+    y:20,
+    opacity:0,
+    color:"red",
+    duration:2,
+    delay:1,
+    stagger:0.2,
+    ease:"bounce"
+})
