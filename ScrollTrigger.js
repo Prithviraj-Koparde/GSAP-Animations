c34971291007d10def64cdee400bf626867891fa
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-08 10:09:23 +0530

    progress

---
+++ ScrollTrigger.js
@@ -0,0 +1,32 @@
+let tl = gsap.timeline();
+tl.from("#page1 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360,
+    ease:"bounce"
+})
+
+tl.from("#page2 h1",{
+    opacity:0,
+    x:500,
+    duration:2,
+    scrollTrigger: {
+        trigger: "#page2 h1",
+        scroller: "body",
+        markers: true,
+        start: "top 50%",
+    }
+})
+
+tl.from("#page2 h2",{
+    opacity:0,
+    x:-500,
+    duration:2,
+    scrollTrigger: {
+        trigger: "#page2 h2",
+        scroller: "body",
+        markers: true,
+        start: "top 50%",
+    }
+})
