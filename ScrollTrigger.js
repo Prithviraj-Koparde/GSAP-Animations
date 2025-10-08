229c6af4c776a8b7b71e8b0b9e287304331353bc
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-08 10:57:43 +0530

    pin and scrub property

--- ScrollTrigger.js
+++ ScrollTrigger.js
@@ -1,9 +1,12 @@
 gsap.to("#page2 h1",{
     transform:"transLateX(-200%)",
     scrollTrigger: {
-        trigger:"#page2 h1",
+        trigger:"#page2", // while using pin always target parent class id not the child
         scroller:"body",
         markers:true,
-        start:"top 50%",
+        start:"top 0%",
+        end:"top -150%",
+        scrub:2,
+        pin:true,
     }
 })
