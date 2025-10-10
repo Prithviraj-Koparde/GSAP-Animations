d8118d972d0988ff8e7913dece52c3fe763f3fd2
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-10 15:42:31 +0530

    ScrollTrigger and cursor practice

--- ScrollTrigger.js
+++ ScrollTrigger.js
@@ -10,3 +10,74 @@ gsap.to("#page2 h1",{
         pin:true,
     }
 })
+
+gsap.from("#page1 #box1",{
+    x:-100,
+    opacity:0,
+    duration: 1,
+    scrollTrigger: {
+        trigger:"#page1 #box1", 
+        scroller:"body",
+        markers: true,
+        start:"top 15%",
+        end:"top 15%",
+    }
+})
+
+gsap.from("#page1 #box2",{
+    x:100,
+    opacity:0,
+    duration: 1,
+    scrollTrigger: {
+        trigger:"#page1 #box2", 
+        scroller:"body",
+        markers: true,
+        start:"top 15%",
+        end:"top 15%",
+    }
+})
+
+let cursor = document.querySelector("#cursor")
+let page1 = document.querySelector("#page1")
+let box1 = document.querySelector("#box1")
+let box2 = document.querySelector("#box2")
+
+page1.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:0.3,
+    })
+})
+
+box1.addEventListener("mouseenter", function(){
+    cursor.innerHTML = "View More"
+    gsap.to(cursor,{
+        scale:4,
+        backgroundColor:"rgba(227, 210, 210, 0.53)"
+    })
+})
+
+box2.addEventListener("mouseenter", function(){
+    cursor.innerHTML = "View More"
+    gsap.to(cursor,{
+        scale:4,
+        backgroundColor:"rgba(227, 210, 210, 0.53)"
+    })
+})
+
+box1.addEventListener("mouseleave", function(){
+    cursor.innerHTML = ""
+    gsap.to(cursor,{
+        scale:1,
+        backgroundColor:"white"
+    })
+})
+
+box2.addEventListener("mouseleave", function(){
+     cursor.innerHTML = ""
+    gsap.to(cursor,{
+        scale:1,
+        backgroundColor:"white"
+    })
+})
